bd1f8bac218b18cf74e379f9d7798e196782cfd2
jrwj97 <jrwj97@aol.com>
2022-05-22 23:42:08 -0400

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ReactPortfolio
+# React Portfolio - Johnny Williams
+  ![Github License](http://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Description
+  This is my portfolio that was built with React!
+  ## Table of Contents
+  * [Installation](#installation)
+  * [License](*license)
+  * [Contributors](#contributors)
+  * [Questions](#questions)
+
+  ## Installation
+  You can clone my portfolio by running "Git clone https://github.com/jrwj97/ReactPortfolio.git"
+
+  ## License
+  This project is licensed under MIT
+
+  ## Contributors
+  jrwj97
+
+  ## Questions
+  If you have any questions about this project, reach out to me at jrwj97@aol.com. You can view my other projects at https://github.com/jrwj97
---
+++ src/portfolio.js
@@ -0,0 +1,63 @@
+const header = {
+  homepage: "",
+  title: "JW.",
+};
+
+const about = {
+  name: "Johnny Williams",
+  role: "Full Stack Engineer",
+  description:
+    "I am a central Florida native who recently made a career change from EMS (Emergency Medical Services) to Web Development. I enrolled in the Full Stack Coding Bootcamp at the University of Central Florida and graduated in early June. I am looking for an entry level oppurtunity to expand on my current skills and knowledge.",
+  resume: "placeholder",
+  social: {
+    linkedin: "https://www.linkedin.com/in/johnny-williamsjr/",
+    github: "https://github.com/jrwj97",
+  },
+};
+
+const projects = [
+  {
+    name: "GitPokemon",
+    description:
+      "GitPokemon is a website that assigns you a random Pokemon sprite depending on how many repos you currently have in your GitHub",
+    stack: ["HTML", "CSS", "JS"],
+    sourceCode: "https://github.com/jrwj97/Pokemon-App",
+    livePreview: "https://jrwj97.github.io/Pokemon-App/",
+  },
+  {
+    name: "Macro-Matics",
+    description:
+      "Macro-Matics makes it easy and convenient to track your daily caloric intake and workouts",
+    stack: ["HTML", "CSS", "JS", "MySQL"],
+    sourceCode: "https://github.com/jrwj97/macro-matics",
+    livePreview: "https://macro-matics.herokuapp.com/",
+  },
+  {
+    name: "Placeholder",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["HTML", "CSS", "JS"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+];
+
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "jQuery",
+  "React",
+  "GraphQL",
+  "MongoDB",
+  "Mongoose",
+  "GitHub",
+  "Node.js",
+  "Jest",
+];
+
+const contact = {
+  email: "jrwj97@aol.com",
+};
+
+export { header, about, projects, skills, contact };
